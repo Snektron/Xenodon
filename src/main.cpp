133039fb972bbad678822ed3ff614a7fa20e6208
Robin Voetter <robin@voetter.nl>
2019-03-11 18:52:34 +0100

    Setup present defines

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@
 #include "graphics/DisplayConfig.h"
 #include "present/Display.h"
 #include "present/Event.h"
-#include "present/xorg/XorgDisplay.h"
-#include "present/Frame.h"
 #include "resources.h"
+
+#if defined(XENODON_PRESENT_XORG)
+    #include "present/xorg/XorgDisplay.h"
+#endif
 
 namespace {
     constexpr const char* const APP_NAME = "Xenodon";
@@ -54,6 +56,70 @@ namespace {
             std::cout << dc << std::endl;
         }
     }
+
+    void run(EventDispatcher& dispatcher, Display* display) {
+        bool quit = false;
+        dispatcher.bind_close([&quit] {
+            quit = true;
+        });
+
+        dispatcher.bind(Key::Escape, [&quit](Action) {
+            quit = true;
+        });
+
+        dispatcher.bind(Key::A, [](Action a) {
+            std::cout << "oof " << (a == Action::Press ? "Press" : "Release") << std::endl;
+        });
+
+        auto start = std::chrono::high_resolution_clock::now();
+        size_t frames = 0;
+
+        while (!quit) {
+            ++frames;
+
+            auto now = std::chrono::high_resolution_clock::now();
+            auto diff = std::chrono::duration<double>(now - start);
+
+            if (diff > std::chrono::seconds{1}) {
+                std::cout << "FPS: " << std::fixed << static_cast<double>(frames) / diff.count() << std::endl;
+                frames = 0;
+                start = now;
+            }
+
+            display->swap_buffers();
+            display->poll_events();
+        }
+    }
+
+    void run_xorg() {
+        #if defined(XENODON_PRESENT_XORG)
+            auto instance = vk::createInstanceUnique(
+                vk::InstanceCreateInfo(
+                    {},
+                    &APP_INFO,
+                    0,
+                    nullptr,
+                    XorgDisplay::REQUIRED_INSTANCE_EXTENSIONS.size(),
+                    XorgDisplay::REQUIRED_INSTANCE_EXTENSIONS.data()
+                )
+            );
+
+            auto dispatcher = EventDispatcher();
+            auto display = XorgDisplay(instance.get(), dispatcher, 800, 600);
+
+            run(dispatcher, &display);
+        #else
+            std::cout << "Xorg support was disabled" << std::endl;
+        #endif
+    }
+
+    void run_direct() {
+        #if defined(XEODON_PRESENT_DIRECT)
+            std::cout << "Direct rendering is WIP" << std::endl;
+        #else
+            std::cout << "Direct support was disabled" << std::endl;
+        #endif
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -66,61 +132,15 @@ int main(int argc, char* argv[]) {
 
     if (subcommand == "help") {
         print_help(argv[0]);
-        return 0;
     } else if (subcommand == "detect") {
         detect();
-        return 0;
-    } else if (subcommand == "test") {
+    } else if (subcommand == "xorg") {
+        run_xorg();
+    } else if (subcommand == "direct") {
+        run_direct();
     } else {
         std::cout << "Error: Invalid subcommand '" << subcommand << "', see `" << argv[0] << " help`" << std::endl;
-        return 0;
     }
 
-    auto instance = vk::createInstanceUnique(
-        vk::InstanceCreateInfo(
-            {},
-            &APP_INFO,
-            0,
-            nullptr,
-            XorgDisplay::REQUIRED_INSTANCE_EXTENSIONS.size(),
-            XorgDisplay::REQUIRED_INSTANCE_EXTENSIONS.data()
-        )
-    );
-
-    auto dispatcher = EventDispatcher();
-    auto display = static_cast<std::unique_ptr<Display>>(
-        std::make_unique<XorgDisplay>(instance.get(), dispatcher, 800, 600)
-    );
-
-    bool quit = false;
-    dispatcher.bind_close([&quit] {
-        quit = true;
-    });
-
-    dispatcher.bind(Key::Escape, [&quit](Action) {
-        quit = true;
-    });
-
-    dispatcher.bind(Key::A, [](Action a) {
-        std::cout << "oof " << (a == Action::Press ? "Press" : "Release") << std::endl;
-    });
-
-    auto start = std::chrono::high_resolution_clock::now();
-    size_t frames = 0;
-
-    while (!quit) {
-        ++frames;
-
-        auto now = std::chrono::high_resolution_clock::now();
-        auto diff = std::chrono::duration<double>(now - start);
-
-        if (diff > std::chrono::seconds{1}) {
-            std::cout << "FPS: " << std::fixed << static_cast<double>(frames) / diff.count() << std::endl;
-            frames = 0;
-            start = now;
-        }
-
-        display->swap_buffers();
-        display->poll_events();
-    }
+    return 0;
 }
