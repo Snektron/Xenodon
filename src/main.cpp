8da422fd2a1dd903fb6c10ecdc42fa5e9323fa2f
Robin Voetter <robin@voetter.nl>
2019-03-11 18:37:21 +0100

    Display detect subcommand

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,21 @@
 #include <string_view>
 #include <chrono>
 #include <iomanip>
+#include <array>
 #include <cstddef>
 #include <vulkan/vulkan.hpp>
+#include "graphics/DisplayConfig.h"
 #include "present/Display.h"
 #include "present/Event.h"
 #include "present/xorg/XorgDisplay.h"
+#include "present/Frame.h"
 #include "resources.h"
 
 namespace {
     constexpr const char* const APP_NAME = "Xenodon";
     constexpr const uint32_t APP_VERSION = VK_MAKE_VERSION(0, 0, 0);
 
-    void print_help(const char* program_name) {
-        std::cout << "Usage: " << program_name << " [options]\n\n"
-            << resources::open("resources/options.txt") << std::endl;
-    }
-}
-
-int main(int argc, char* argv[]) {
-    for (size_t i = 1; i < static_cast<size_t>(argc); ++i) {
-        auto arg = std::string_view(argv[i]);
-        if (arg == "-h" || arg == "--help") {
-            print_help(argv[0]);
-            return 0;
-        } else {
-            std::cerr << "Invalid argument '" << arg << "'.\nUse "
-                << argv[0] << " --help for more information on usage." << std::endl;
-            return 0;
-        }
-    }
-
-    const auto app_info = vk::ApplicationInfo(
+    const auto APP_INFO = vk::ApplicationInfo(
         APP_NAME,
         APP_VERSION,
         nullptr,
@@ -40,10 +24,62 @@ int main(int argc, char* argv[]) {
         VK_API_VERSION_1_1
     );
 
+    void print_help(const char* program_name) {
+        std::cout
+            << "Usage:\n    " << program_name << " [subcommand] [flags]\n\n"
+            << resources::open("resources/help.txt") << std::endl;
+    }
+
+    void detect() {
+        constexpr const std::array required_instance_extensions = {
+            VK_KHR_DISPLAY_EXTENSION_NAME
+        };
+
+        auto instance = vk::createInstanceUnique(
+            vk::InstanceCreateInfo(
+                {},
+                &APP_INFO,
+                0,
+                nullptr,
+                required_instance_extensions.size(),
+                required_instance_extensions.data()
+            )
+        );
+
+        auto dc = DisplayConfig::auto_detect(instance.get());
+
+        if (dc.gpus.empty()) {
+            std::cout << "No displays detected" << std::endl;
+        } else {
+            std::cout << dc << std::endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        std::cout << "Error: Subcommand required, see `" << argv[0] << " help`" << std::endl;
+        return 0;
+    }
+
+    auto subcommand = std::string_view(argv[1]);
+
+    if (subcommand == "help") {
+        print_help(argv[0]);
+        return 0;
+    } else if (subcommand == "detect") {
+        detect();
+        return 0;
+    } else if (subcommand == "test") {
+    } else {
+        std::cout << "Error: Invalid subcommand '" << subcommand << "', see `" << argv[0] << " help`" << std::endl;
+        return 0;
+    }
+
     auto instance = vk::createInstanceUnique(
         vk::InstanceCreateInfo(
             {},
-            &app_info,
+            &APP_INFO,
             0,
             nullptr,
             XorgDisplay::REQUIRED_INSTANCE_EXTENSIONS.size(),
