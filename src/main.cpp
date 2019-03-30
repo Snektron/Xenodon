30012c9887d59f890ac5b3fc0f48e29869576955
Robin Voetter <robin@voetter.nl>
2019-03-30 16:28:19 +0100

    Add PCI bus info to sysinfo command

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,10 @@ namespace {
             )
         );
 
+        auto getPhysicalDeviceProperties2 = reinterpret_cast<PFN_vkGetPhysicalDeviceProperties2>(
+            instance->getProcAddr("vkGetPhysicalDeviceProperties2")
+        );
+
         fmt::print("System setup information\n");
         auto gpus = instance->enumeratePhysicalDevices();
         if (gpus.empty()) {
@@ -50,13 +54,40 @@ namespace {
         }
 
         for (size_t i = 0; i < gpus.size(); ++i) {
-            auto props = gpus[i].getProperties();
-            fmt::print(
-                "GPU {}:\n\tname: '{}'\n\ttype: {}\n",
-                i,
-                props.deviceName,
-                vk::to_string(props.deviceType)
+            fmt::print("GPU {}:\n", i);
+
+            auto pci_info = vk::PhysicalDevicePCIBusInfoPropertiesEXT();
+            auto props2 = vk::PhysicalDeviceProperties2();
+
+            const auto extensions = gpus[i].enumerateDeviceExtensionProperties();
+            for (const auto& extension : extensions) {
+                if (std::string_view(VK_EXT_PCI_BUS_INFO_EXTENSION_NAME) == extension.extensionName) {
+                    props2.pNext = static_cast<void*>(&pci_info);
+                    break;
+                }
+            }
+
+            getPhysicalDeviceProperties2(
+                static_cast<VkPhysicalDevice>(gpus[i]),
+                reinterpret_cast<VkPhysicalDeviceProperties2*>(&props2)
             );
+
+            fmt::print(
+                "\tname: '{}'\n\ttype: {}\n",
+                props2.properties.deviceName,
+                vk::to_string(props2.properties.deviceType)
+            );
+
+            if (props2.pNext) {
+                fmt::print(
+                    "\tpci bus: {:0>8}:{:0>2}:{:0>2}.{}\n",
+                    pci_info.pciDomain,
+                    pci_info.pciBus,
+                    pci_info.pciDevice,
+                    pci_info.pciFunction);
+            } else {
+                fmt::print("\tskipping bus info (extension not present)\n");
+            }
 
             auto display_props = gpus[i].getDisplayPropertiesKHR();
 
